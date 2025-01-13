a229b4732c2e698ae25608d3a875bf21cca1dd34
KM Lee <149044616+IlllIIlII@users.noreply.github.com>
2025-01-13 19:10:01 +0900

    Add files via upload

---
+++ 2(1)-Python/submission/5670.py
@@ -0,0 +1,108 @@
+from dataclasses import dataclass, field
+from typing import TypeVar, Generic, Optional, Iterable
+
+
+"""
+TODO:
+- Trie.push 구현하기
+- (필요할 경우) Trie에 추가 method 구현하기
+"""
+
+
+T = TypeVar("T")
+
+
+@dataclass
+class TrieNode(Generic[T]):
+    body: Optional[T] = None
+    children: list[int] = field(default_factory=lambda: [])
+    is_end: bool = False
+
+
+class Trie(list[TrieNode[T]]):
+    def __init__(self) -> None:
+        super().__init__()
+        self.append(TrieNode(body=None))
+
+    def push(self, seq: Iterable[T]) -> None:
+        """
+        seq: T의 열 (list[int]일 수도 있고 str일 수도 있고 등등...)
+
+        action: trie에 seq을 저장하기
+        """
+        # 구현하세요!
+        current_node = self[0]
+        for char in seq:
+            if char not in current_node.children:
+                new_node = TrieNode(body=char)
+                self.append(new_node)
+                current_node.children.append(len(self) - 1)
+            current_node = self[current_node.children[-1]]
+        current_node.is_end = True
+        pass
+            
+       
+
+    # 구현하세요!
+    def calculate_order(self, node_idx: int = 0, mod: int = 1000000007) -> int:
+        """
+        node_idx: 현재 계산 중인 노드의 인덱스
+        mod: 계산 중에 결과를 나눌 값 1000000007
+        """
+        current_node = self[node_idx]
+        result = 1
+
+        for child_idx in current_node.children:
+            result *= self.calculate_order(child_idx, mod)
+            result %= mod
+
+    
+        child_count = len(current_node.children)
+        if child_count > 1:  
+            for i in range(1, child_count + 1):
+                result *= i
+                result %= mod
+
+        return result
+
+
+
+import sys
+
+
+"""
+TODO:
+- 일단 Trie부터 구현하기
+- count 구현하기
+- main 구현하기
+"""
+
+
+def count(trie: Trie, query_seq: str) -> int:
+    """
+    trie - 이름 그대로 trie
+    query_seq - 단어 ("hello", "goodbye", "structures" 등)
+
+    returns: query_seq의 단어를 입력하기 위해 버튼을 눌러야 하는 횟수
+    """
+    pointer = 0
+    cnt = 0
+
+    for element in query_seq:
+        if len(trie[pointer].children) > 1 or trie[pointer].is_end:
+            cnt += 1
+
+        new_index = None # 구현하세요!
+
+        pointer = new_index
+
+    return cnt + int(len(trie[0].children) == 1)
+
+
+def main() -> None:
+    # 구현하세요!
+    pass
+
+
+if __name__ == "__main__":
+    main()
